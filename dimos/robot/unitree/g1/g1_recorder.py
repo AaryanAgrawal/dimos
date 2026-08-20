ac4def9f96507bdb18af59f0458879f9e5994d67
Andrew Lauer <andrewcl@email.unc.edu>
2026-08-20 10:43:46 -0700

    lz4 instead of jpeg

--- dimos/robot/unitree/g1/g1_recorder.py
+++ dimos/robot/unitree/g1/g1_recorder.py
@@ -16,13 +16,24 @@
 
 from __future__ import annotations
 
+from pydantic import Field
+
 from dimos.core.stream import In
-from dimos.hardware.sensors.lidar.pointlio.recorder import PointlioRecorder
+from dimos.hardware.sensors.lidar.pointlio.recorder import PointlioRecorder, PointlioRecorderConfig
 from dimos.msgs.sensor_msgs.CameraInfo import CameraInfo
 from dimos.msgs.sensor_msgs.Image import Image
 
 
+class G1RecorderConfig(PointlioRecorderConfig):
+    # don't compress
+    stream_codecs: dict[str, str] = Field(
+        default_factory=lambda: {"realsense_depth_image": "lz4+lcm"}
+    )
+
+
 class G1Recorder(PointlioRecorder):
+    config: G1RecorderConfig
+
     color_image: In[Image]
     realsense_depth_image: In[Image]
     realsense_camera_info: In[CameraInfo]
